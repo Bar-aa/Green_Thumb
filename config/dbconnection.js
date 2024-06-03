a765c874561f89d583305d0af6a99bbfdc7ea330
BaraaShelbaya <baraa.shellbaya@gmail.com>
2024-06-03 09:29:58 +0300

    adding all libraryes

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,11 +1,11 @@
-const {DATABASE, DATABASE_HOST, DATABASE_USER, DATABASE_PAEEWORD} = process.env;
+const {DATABASE, DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD} = process.env;
 
 var mysql = require("mysql");
 
 var conn = mysql.createConnection({
     host: DATABASE_HOST,
     user: DATABASE_USER,
-    password: DATABASE_PAEEWORD,
+    password: DATABASE_PASSWORD,
     database: DATABASE
 });
 
