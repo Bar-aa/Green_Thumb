ca4b5098eb9b12b40c8f7cf1b18e88ac30004a5b
samaayasin <samaayasin85@gmail.com>
2024-06-11 09:21:02 +0300

    update

--- routes/signinAuthRoute.js
+++ routes/signinAuthRoute.js
@@ -8,7 +8,6 @@ const router = express.Router();
 router.post('/signin', authenticate, (req, res) => {
     const { username, password } = req.body;
 
-    // Fetch user from the database by username
     getUserByUsername(username, (err, user) => {
         if (err) {
             console.error('Database error:', err);
@@ -18,13 +17,11 @@ router.post('/signin', authenticate, (req, res) => {
             return res.status(401).json({ message: 'Invalid username' });
         }
 
-        // Hash input password and compare with stored password
         const hashedInputPassword = crypto.createHash('sha1').update(password).digest('hex');
         if (hashedInputPassword !== user.sha1_password) {
             return res.status(401).json({ message: 'Invalid password' });
         }
 
-        // Authentication successful
         res.status(200).json({ message: 'Sign in successful' });
     });
 });
