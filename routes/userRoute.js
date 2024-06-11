b77a4f450e9e15005c7dc3e63a99909beeffa11e
samaayasin <samaayasin85@gmail.com>
2024-06-11 19:40:05 +0300

    User APIs

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const {
-    getUserById
+    getAllUsers,
+    getUserById,
+    getUsersByName,
+    createUser,
+    updateUser,
+    deleteUser
 } = require('../Services/user');
 
 const {   
-    validateUserID
-    
-    
+    validateUserID,
+    validateUserCreation,
+    validateUserUpdate   
 } = require('../Validation/userValidation');
+router.get('/', getAllUsers);
+router.get('/:user_id', validateUserID, getUserById);
+router.get('/name/:name', getUsersByName);
+router.post('/', validateUserCreation, createUser);
+router.put('/:id', validateUserID, validateUserUpdate, updateUser);
+router.delete('/:id', validateUserID, deleteUser);
 
-router.get('/:user_id',validateUserID, getUserById);
 
 module.exports = router;
